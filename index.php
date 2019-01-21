1b491c7fe63c5e1f36bb34679868681cf9497678
Volodymyr Zhonchuk <vzhonchuk@gmail.com>
2019-01-21 23:57:53 +0200

    Logo font change, button changed to links, button up revamped, button up script deleted

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-  <link href="https://fonts.googleapis.com/css?family=PT+Serif|Roboto|Rubik|Shrikhand" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Oswald|Roboto|Rubik" rel="stylesheet">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
   <link rel="stylesheet" href="css/main.css">
 
@@ -34,13 +34,16 @@
           <a class="nav-link js-scroll-trigger" href="#section2">about</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link js-scroll-trigger" href="#section3">link</a>
+          <a class="nav-link js-scroll-trigger" href="#section3">skills</a>
         </li>
         <li class="nav-item">
           <a class="nav-link js-scroll-trigger" href="#section4">portfolio</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link js-scroll-trigger" href="#section5">contacts</a>
+          <a class="nav-link js-scroll-trigger" href="#section5">testimonials</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link js-scroll-trigger" href="#section6">contacts</a>
         </li>
       </ul>
     </div>
@@ -55,8 +58,8 @@
       <figcaption>
         <h2>Alonso Media. Deserve a better code</h2>
         <h6>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae quas aspernatur libero sint ratione facilis iste et! Eligendi, nesciunt, cumque?</h6>
-        <button type="button" name="button" class="button">About me</button>
-        <button type="button" name="button">Contact me</button>
+        <a href="#section2" class="about_me_link js-scroll-trigger">About me</a>
+        <a href="#section5" class="contact_me_link js-scroll-trigger">Contact me</a>
       </figcaption>
     </figure>
 
@@ -71,105 +74,95 @@
 
     <div class="advantages-wrapper">
 
+      <div class="left_advantage">
 
-          <div class="left_advantage">
-
-            <div class="advantage_item">
-              <div class="advantage_text">
-                <h6>Balance Body & Mind</h6>
-                <p>dolor sit amet, consectetur adipisicing elit. Beatae quas aspernatur libero</p>
-              </div>
-              <div class="advantage_icon">
-                <figure>
-                  <img src="img/responsive.png" alt="">
-                </figure>
-              </div>
-            </div>
-
-            <div class="advantage_item">
-              <div class="advantage_text">
-                <h6>Balance Body & Mind</h6>
-                <p>dolor sit amet, consectetur adipisicing elit. Beatae quas aspernatur libero</p>
-              </div>
-              <div class="advantage_icon">
-                <figure>
-                  <img src="img/hourglass.png" alt="">
-                </figure>
-              </div>
-            </div>
-
-            <div class="advantage_item">
-              <div class="advantage_text">
-                <h6>Balance Body & Mind</h6>
-                <p>dolor sit amet, consectetur adipisicing elit. Beatae quas aspernatur libero</p>
-              </div>
-              <div class="advantage_icon">
-                <figure>
-                  <img src="img/customer-support.png" alt="">
-                </figure>
-              </div>
-            </div>
+        <div class="advantage_item">
+          <div class="advantage_text">
+            <h6>Balance Body & Mind</h6>
+            <p>dolor sit amet, consectetur adipisicing elit. Beatae quas aspernatur libero</p>
           </div>
-
-
-
-
-          <div class="advantage_portrait">
-            <img src="img/businessman.jpg" alt="">
-          </div>
-
-
-
-        <div class="right_advantage">
-
-          <div class="advantage_item">
-            <div class="advantage_icon">
-              <figure>
-                <img src="img/innovation.png" alt="">
-              </figure>
-            </div>
-            <div class="advantage_text">
-              <h6>Balance Body & Mind</h6>
-              <p>dolor sit amet, consectetur adipisicing elit. Beatae quas aspernatur libero</p>
-            </div>
-          </div>
-
-          <div class="advantage_item">
-            <div class="advantage_icon">
-              <figure>
-                <img src="img/layers.png" alt="">
-              </figure>
-            </div>
-            <div class="advantage_text">
-              <h6>Balance Body & Mind</h6>
-              <p>dolor sit amet, consectetur adipisicing elit. Beatae quas aspernatur libero</p>
-            </div>
-          </div>
-
-          <div class="advantage_item">
-            <div class="advantage_icon">
-              <figure>
-                <img src="img/settings.png" alt="">
-              </figure>
-            </div>
-            <div class="advantage_text">
-              <h6>Balance Body & Mind</h6>
-              <p>dolor sit amet, consectetur adipisicing elit. Beatae quas aspernatur libero</p>
-            </div>
+          <div class="advantage_icon">
+            <figure>
+              <img src="img/responsive.png" alt="">
+            </figure>
           </div>
         </div>
 
+        <div class="advantage_item">
+          <div class="advantage_text">
+            <h6>Balance Body & Mind</h6>
+            <p>dolor sit amet, consectetur adipisicing elit. Beatae quas aspernatur libero</p>
+          </div>
+          <div class="advantage_icon">
+            <figure>
+              <img src="img/hourglass.png" alt="">
+            </figure>
+          </div>
+        </div>
 
+        <div class="advantage_item">
+          <div class="advantage_text">
+            <h6>Balance Body & Mind</h6>
+            <p>dolor sit amet, consectetur adipisicing elit. Beatae quas aspernatur libero</p>
+          </div>
+          <div class="advantage_icon">
+            <figure>
+              <img src="img/customer-support.png" alt="">
+            </figure>
+          </div>
+        </div>
+      </div>
 
+      <div class="advantage_portrait">
+        <img src="img/businessman.jpg" alt="">
+      </div>
 
+      <div class="right_advantage">
+
+        <div class="advantage_item">
+          <div class="advantage_icon">
+            <figure>
+              <img src="img/innovation.png" alt="">
+            </figure>
+          </div>
+          <div class="advantage_text">
+            <h6>Balance Body & Mind</h6>
+            <p>dolor sit amet, consectetur adipisicing elit. Beatae quas aspernatur libero</p>
+          </div>
+        </div>
+
+        <div class="advantage_item">
+          <div class="advantage_icon">
+            <figure>
+              <img src="img/layers.png" alt="">
+            </figure>
+          </div>
+          <div class="advantage_text">
+            <h6>Balance Body & Mind</h6>
+            <p>dolor sit amet, consectetur adipisicing elit. Beatae quas aspernatur libero</p>
+          </div>
+        </div>
+
+        <div class="advantage_item">
+          <div class="advantage_icon">
+            <figure>
+              <img src="img/settings.png" alt="">
+            </figure>
+          </div>
+          <div class="advantage_text">
+            <h6>Balance Body & Mind</h6>
+            <p>dolor sit amet, consectetur adipisicing elit. Beatae quas aspernatur libero</p>
+          </div>
+        </div>
+      </div>
 
     </div>
   </section>
 
   <section id="section3" class="container-fluid">
 
-      <div class="skills_wrapper">
-          <div class="skills_image">
+    <div class="skills_wrapper">
+      <div class="skills_image">
         <img src="img/skills.jpg" alt="skills">
       </div>
 
@@ -223,7 +216,7 @@
         <!-- END- SKILLS -->
       </div>
 
-  </div>
+    </div>
 
   </section>
 
@@ -233,10 +226,10 @@
     <div class="filters filter_button_group">
       <ul>
 
-      <li class="active" data-filter="*">All projects</li>
-      <li data-filter=".metal">Wordpress/Woocommerce</li>
-      <li data-filter=".transition">Yii2</li>
-      <li data-filter=".alkali">VueJs</li>
+        <li class="active" data-filter="*">All projects</li>
+        <li data-filter=".metal">Wordpress/Woocommerce</li>
+        <li data-filter=".transition">Yii2</li>
+        <li data-filter=".alkali">VueJs</li>
       </ul>
     </div>
 
@@ -284,7 +277,7 @@
     <div id="caption"></div>
   </div>
 
-  <section id="section5" class="container-fluid">
+  <section id="section5" class="container-fluid testimonials">
     <div class="testimonials_wrapper">
       <h3>Testimonials</h3>
 
@@ -320,116 +313,116 @@
     </div>
   </section>
 
-<section class="techologies" data-type="background">
-  <div class="techologies_background">
-  <div class="techologies_wrapper">
+  <section class="techologies" data-type="background">
+    <div class="techologies_background">
+      <div class="techologies_wrapper">
 
 
 
-  <div class="techologies_image_holder">
-<img src="img/php.png" alt="php">
-</div>
+        <div class="techologies_image_holder">
+          <img src="img/php.png" alt="php">
+        </div>
 
-<div class="techologies_image_holder">
-<img src="img/vue.png" alt="vue">
-</div>
+        <div class="techologies_image_holder">
+          <img src="img/vue.png" alt="vue">
+        </div>
 
-<div class="techologies_image_holder">
-<img src="img/yii.png" alt="yii">
-</div>
+        <div class="techologies_image_holder">
+          <img src="img/yii.png" alt="yii">
+        </div>
 
-<div class="techologies_image_holder">
-<img src="img/docker.png" alt="docker">
-</div>
+        <div class="techologies_image_holder">
+          <img src="img/docker.png" alt="docker">
+        </div>
 
-<div class="techologies_image_holder">
-<img src="img/wordpress.png" alt="wordpress">
-</div>
+        <div class="techologies_image_holder">
+          <img src="img/wordpress.png" alt="wordpress">
+        </div>
 
-<div class="techologies_image_holder">
-<img src="img/woocommerce.png" alt="woocommerce">
-</div>
+        <div class="techologies_image_holder">
+          <img src="img/woocommerce.png" alt="woocommerce">
+        </div>
 
-<div class="techologies_image_holder">
-<img src="img/gulp.png" alt="gulp">
-</div>
+        <div class="techologies_image_holder">
+          <img src="img/gulp.png" alt="gulp">
+        </div>
 
-<div class="techologies_image_holder">
-<img src="img/vagrant.png" alt="vagrant">
-</div>
+        <div class="techologies_image_holder">
+          <img src="img/vagrant.png" alt="vagrant">
+        </div>
 
-  </div>
-</section>
-
-<section class="contact">
-  <div class="contact-wrapper">
-    <div class="contact_details">
-      <h6>Address</h6>
-      <p>10 Downing Street</p>
-      <p>London</p>
-      <p>SW1A 2AA</p>
-      <hr>
-      <h6>Phone</h6>
-      <p>Landline: +38 044 553 8848</p>
-      <p>Mobile: +38 066 455 7032</p>
-      <hr>
-      <h6>Email</h6>
-      <p>contact@alonso.com</p>
-      <p>support@alonso.com</p>
-    </div>
-
-
-
-
-    <form class="contact_form" action="" method="post">
-      <div class="name_email_subject">
-
-        <input type="text" name="" value="" placeholder="Name">
-        <input type="text" name="" value="" placeholder="Subject">
-        <input type="email" name="" value="" placeholder="Email">
       </div>
+    </section>
 
-      <div class="message">
-        <input type="textarea" name="" value="" placeholder="Message">
+    <section id="section6" class="contact">
+      <div class="contact-wrapper">
+        <div class="contact_details">
+          <h6>Address</h6>
+          <p>10 Downing Street</p>
+          <p>London</p>
+          <p>SW1A 2AA</p>
+          <hr>
+          <h6>Phone</h6>
+          <p>Landline: +38 044 553 8848</p>
+          <p>Mobile: +38 066 455 7032</p>
+          <hr>
+          <h6>Email</h6>
+          <p>contact@alonso.com</p>
+          <p>support@alonso.com</p>
+        </div>
+
+
+
+
+        <form class="contact_form" action="" method="post">
+          <div class="name_email_subject">
+
+            <input type="text" name="" value="" placeholder="Name">
+            <input type="text" name="" value="" placeholder="Subject">
+            <input type="email" name="" value="" placeholder="Email">
+          </div>
+
+          <div class="message">
+            <input type="textarea" name="" value="" placeholder="Message">
+          </div>
+          <input type="submit" name="" value="Submit">
+        </form>
       </div>
-      <input type="submit" name="" value="Submit">
-  </form>
+    </section>
+
+    <footer>
+
+      <div class="footer_wrapper">
+
+        <div class="copyright">
+          <p>&copy;Alonso. All rights reserved. 2019</p>
+        </div>
+
+        <div class="social">
+          <a href="#"><i class="fab fa-facebook-f"></i></a>
+          <a href="#"><i class="fab fa-twitter"></i></a>
+          <a href="#"><i class="fab fa-codepen"></i></a>
+          <a href="#"><i class="fab fa-github"></i></a>
+        </div>
+
+        <div class="button_up">
+          <a href="#section1" class="btn-up js-scroll-trigger">Back to top<i class="fas fa-angle-up"></i></a>
+        </div>
+
       </div>
-</section>
+    </footer>
 
-<footer>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="js/jquery.easing.js">
+    </script>
+    <script src="https://unpkg.com/isotope-layout@3/dist/isotope.pkgd.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
 
-    <div class="footer_wrapper">
+    <script type="text/javascript" src="js/main.js">
 
-      <div class="copyright">
-    <p>&copy;Alonso. All rights reserved. 2019</p>
-    </div>
-
-    <div class="social">
-  <a href="#"><i class="fab fa-facebook-f"></i></a>
-  <a href="#"><i class="fab fa-twitter"></i></a>
-  <a href="#"><i class="fab fa-codepen"></i></a>
-  <a href="#"><i class="fab fa-github"></i></a>
-  </div>
-
-  <div class="button_up">
-<button class="btn-up">Back to top<i class="fas fa-angle-up"></i></button>
-</div>
-
-  </div>
-</footer>
-
-<!-- Optional JavaScript -->
-<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
-<script type="text/javascript" src="js/jquery.easing.js">
-</script>
-<script src="https://unpkg.com/isotope-layout@3/dist/isotope.pkgd.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
-
-<script type="text/javascript" src="js/main.js">
-
-</script>
-</body>
-</html>
+    </script>
+  </body>
+  </html>
--- js/main.js
+++ js/main.js
@@ -1,37 +1,37 @@
 $( document ).ready( function() {
 
   // Smooth scrolling using jQuery easing
-    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-        if (target.length) {
-          $('html, body').animate({
-            scrollTop: (target.offset().top - 126)
-          }, 1000, "easeInOutExpo");
-          return false;
-        }
-      }
-    });
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+  	if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+  		var target = $(this.hash);
+  		target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+  		if (target.length) {
+  			$('html, body').animate({
+  				scrollTop: (target.offset().top - 126)
+  			}, 1000, "easeInOutExpo");
+  			return false;
+  		}
+  	}
+  });
 
     // Closes responsive menu when a scroll trigger link is clicked
-      $('.js-scroll-trigger').click(function() {
-        $('.navbar-collapse').collapse('hide');
-      });
+    $('.js-scroll-trigger').click(function() {
+    	$('.navbar-collapse').collapse('hide');
+    });
 
   // Activate scrollspy to add active class to navbar items on scroll
   $('body').scrollspy({
-    target: '#mainNav',
-    offset: 57
+  	target: '#mainNav',
+  	offset: 57
   });
 
   // Collapse Navbar
   var navbarCollapse = function() {
-    if ($("#mainNav").offset().top > 100) {
-      $("#mainNav").addClass("navbar-shrink");
-    } else {
-      $("#mainNav").removeClass("navbar-shrink");
-    }
+  	if ($("#mainNav").offset().top > 100) {
+  		$("#mainNav").addClass("navbar-shrink");
+  	} else {
+  		$("#mainNav").removeClass("navbar-shrink");
+  	}
   };
   // Collapse now if page is not at top
   navbarCollapse();
@@ -41,26 +41,26 @@ $( document ).ready( function() {
 
 // Skillbar animation
 $(document).on('scroll', function(){
-    if($('html,body').scrollTop() > $('#skills').height()){
-  $(".skillbar").each(function(){
+	if($('html,body').scrollTop() > $('#skills').height()){
+		$(".skillbar").each(function(){
 			$(this).find(".skillbar-bar").animate({
 				width:$(this).attr("data-percent")
 			},3000);
 		});
-  }
+	}
 });
 
 var $grid = $('.portfolio_container').isotope({
-  itemSelector: '.portfolio_item',
-  stagger: 30
+	itemSelector: '.portfolio_item',
+	stagger: 30
 });
 
 $('.filter_button_group').on( 'click', 'li', function() {
-  var filterValue = $(this).attr('data-filter');
-  $grid.isotope({ filter: filterValue });
+	var filterValue = $(this).attr('data-filter');
+	$grid.isotope({ filter: filterValue });
 
-  $('.filter_button_group li').removeClass('active');
-  $(this).addClass('active');
+	$('.filter_button_group li').removeClass('active');
+	$(this).addClass('active');
 });
 
 
@@ -68,37 +68,32 @@ $('.filter_button_group').on( 'click', 'li', function() {
 
 
 //Parallax
-    var $window = $(window);
-    if($('section[data-type="background"]').length){
-        $('section[data-type="background"]').each(function(){
+var $window = $(window);
+if($('section[data-type="background"]').length){
+	$('section[data-type="background"]').each(function(){
 
-            var $obj = $(this);
-            var offset = $obj.offset().top;
+		var $obj = $(this);
+		var offset = $obj.offset().top;
 
-            $(window).scroll(function()
-            {
-                offset = $obj.offset().top;
+		$(window).scroll(function()
+		{
+			offset = $obj.offset().top;
 
-                if ($window.scrollTop() > (offset - window.innerHeight))
-                {
-                    var yPos = -(($window.scrollTop() - offset) / 2 );
-                    var coords = '50% ' + ( yPos ) + 'px';
-                    $obj.css({ backgroundPosition:  coords });
-                }
-            });
-            $(window).resize(function()
-            {
-                offset = $obj.offset().top;
-            });
-        });
-    }
+			if ($window.scrollTop() > (offset - window.innerHeight))
+			{
+				var yPos = -(($window.scrollTop() - offset) / 2 );
+				var coords = '50% ' + ( yPos ) + 'px';
+				$obj.css({ backgroundPosition:  coords });
+			}
+		});
+		$(window).resize(function()
+		{
+			offset = $obj.offset().top;
+		});
+	});
+}
 
-    // Button Up
-     var $btnUp = $(".btn-up")
 
-     $btnUp.on("click", function(){
-       $("html,body").animate({scrollTop:0}, 900)
-     });
 
 });
 
@@ -107,7 +102,7 @@ var modal = document.getElementById('portfolioModal');
 var span = $(".close");
 
 span.on("click", function() {
-    modal.style.display = "none";
+	modal.style.display = "none";
 });
 
 var images = document.getElementsByClassName("modal_image");
@@ -115,10 +110,10 @@ var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
 var i;
 for (i = 0; i < images.length; i++) {
-  images[i].onclick = function() {
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    modalImg.alt = this.alt;
-    captionText.innerHTML = this.alt;
-  };
+	images[i].onclick = function() {
+		modal.style.display = "block";
+		modalImg.src = this.src;
+		modalImg.alt = this.alt;
+		captionText.innerHTML = this.alt;
+	};
 }
