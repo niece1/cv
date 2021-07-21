e72f37babbb72af0677d731c006b72fe124abe1a
Volodymyr Zhonchuk <vzhonchuk@gmail.com>
2021-07-21 21:41:45 +0300

    :construction_worker: CI: PHP cs and Travis
    Code sniffer and Travis added

--- src/Models/Contact.php
+++ src/Models/Contact.php
@@ -6,31 +6,30 @@ public static function checkEmail()
 {
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
         // Replace this email with recipient email
         $mail_to = "vzhonchuk@gmail.com";
-        
+
         // Sender Data
         $subject = trim($_POST["subject"]);
-        $name = str_replace(array("\r","\n"),array(" "," ") , strip_tags(trim($_POST["name"])));
+        $name = str_replace(array("\r","\n"), array(" "," "), strip_tags(trim($_POST["name"])));
         $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
-        
+
         $message = trim($_POST["message"]);
-        
-        if ( empty($name) OR !filter_var($email, FILTER_VALIDATE_EMAIL) OR empty($subject) OR empty($message)) {
+
+        if (empty($name) or !filter_var($email, FILTER_VALIDATE_EMAIL) or empty($subject) or empty($message)) {
             // Set a 400 (bad request) response code and exit.
             http_response_code(400);
             echo "Please complete the form and try again.";
             exit;
         }
-        
+
         # Mail Content
         $content = "Name: $name\n";
         $content .= "Email: $email\n\n";
         $content .= "Phone: $phone\n";
         $content .= "Message:\n$message\n";
 
-        
+
         // email headers.
         $headers = "From: $name &lt;$email&gt;";
 
@@ -45,11 +44,9 @@ public static function checkEmail()
             http_response_code(500);
             echo "Oops! Something went wrong, we couldn't send your message.";
         }
-
-        } else {
-            // Not a POST request, set a 403 (forbidden) response code.
-            http_response_code(403);
-            echo "There was a problem with your submission, please try again.";
-        }
+    } else {
+        // Not a POST request, set a 403 (forbidden) response code.
+        http_response_code(403);
+        echo "There was a problem with your submission, please try again.";
     }
-?>
+}
